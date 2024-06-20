e44f01b621f7631e7bd519f4208caad5a467f7ab
Daria <111975044+Didileon@users.noreply.github.com>
2024-06-20 23:58:23 +0300

    Add files via upload

--- mywebsite/chat/views.py
+++ mywebsite/chat/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def lobby(request):
+    # return render(request, 'lobby.html')
     return render(request, 'chat/lobby.html')
--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -47,7 +47,10 @@ INSTALLED_APPS = [
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND':'channels.layers.InMemoryChannelLayer'
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'MYWEBSITE': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
     }
 }
 
